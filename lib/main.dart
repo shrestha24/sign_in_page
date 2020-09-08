871ffa8142ad782bbcd03f962e028a34956a88f8
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2020-09-08 13:25:52 +0530

    sign_in updated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,86 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.amberAccent,
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage(''),
-              ),
-              Text(
-                'Shrestha Sonali',
-                style: TextStyle(
-                  color: Colors.lightBlueAccent,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24.0,
-                ),
-              ),
-              Text(
-                'Flutter Developer',
-                style: TextStyle(
-                  color: Colors.lightBlueAccent.shade100,
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0,
-                horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                        Icons.phone,
-                    color: Colors.amber,
-                    ),
-                    Text(
-                      ' 91+ 6832647367',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.blueAccent.shade100,
-
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                color: Colors.white,
-                  padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0,
-                horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.amberAccent.shade200,
-                    ),
-                    Text(
-                      'Shrestha@gmail.com',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.lightBlueAccent.shade200,
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new MyHomePage(),
     );
   }
 }
+   class MyHomePage extends StatefulWidget {
+  @override
+   _MyHomePageState createState() => new _MyHomePageState();
+ }
+
+  class _MyHomePageState extends State<MyHomePage> {
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 90.0, 0.0, 0.0),
+                child: Text(
+                  'Hello',
+                  style: TextStyle(
+                    fontSize: 50.0, fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 140.0, 0.0, 0.0),
+                  child: Text(
+                    'There',
+                    style: TextStyle(
+                      fontSize: 50.0, fontWeight: FontWeight.bold)
+                    ),
+                  ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'EMAIL',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                    ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green)
+                      )
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'PASSWORD',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)
+                      )
+                  ),
+                  obscureText: true,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  }
