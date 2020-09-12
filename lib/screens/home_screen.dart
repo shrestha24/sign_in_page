11af00a1ad97d43b5cadcca7503accb421a02de2
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2020-09-12 23:25:40 +0530

    App bar with hamburger.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.menu),
@@ -38,6 +39,18 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: <Widget>[
           CategorySelector(),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+
+            ),
+          )
         ],
       ),
     );
--- lib/widgets/category_selector.dart
+++ lib/widgets/category_selector.dart
@@ -14,17 +14,31 @@ class _CategorySelectorState extends State<CategorySelector> {
   Widget build(BuildContext context) {
     return Container(
       height: 90.0,
-      color: Colors.blue,
+      color: Theme.of(context).primaryColor,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
           itemCount: categories.length,
           itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 20.0,
-                vertical: 30.0,
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 30.0,
+                ),
+                child: Text(
+                  categories[index],
+                style: TextStyle(
+                  color: index == selectedIndex ? Colors.white : Colors.white60,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.2,
+                ),),
               ),
-              child: Text(categories[index]),
             );
     },
       ),
